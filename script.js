9aabe3c694fa622522f845cbe93246dccf6efb61
Nadir <47250096+lenadir@users.noreply.github.com>
2025-09-06 10:56:59 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// auto-update year
+document.getElementById("year").textContent = new Date().getFullYear();
+
+// dark mode toggle
+const toggle = document.getElementById("themeToggle");
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  toggle.textContent = document.body.classList.contains("dark") ? "☀️" : "🌙";
+});
